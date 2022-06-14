235b1c7ab22876c46204fedbcd98536a48d410e4
castlea121 <adam.castle@gridsmart.co.uk>
2022-06-14 22:17:43 +0100

    Lesson 54

--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = 'you are good';
+    } else if (resultScore <= 12) {
+      resultText = 'you are okay';
+    } else if (resultScore <= 16) {
+      resultText = 'you are bad';
+    } else {
+      resultText = 'you are very bad';
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it!'),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+      ),
     );
   }
 }
